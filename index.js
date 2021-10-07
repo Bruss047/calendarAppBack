03687d85ea86a325fb0d3023f7ddff53fda05165
Javier Bruzzese <bruzzesejavier@gmail.com>
2021-10-07 18:15:08 -0300

    Modificación del index

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 require('dotenv').config(); // Configura las variables de entorno.
 const {dbConnection} = require('./database/config');
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
+const path = require('path');
 
 //Base de Datos:
 dbConnection();
@@ -18,6 +19,11 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
+
+
+app.get('*', ( req, res ) => {
+    res.sendFile( path.join( __dirname+'/public/index.html' ) );
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
